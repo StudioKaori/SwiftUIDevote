4a1dcc46a6a15adf28f95da6aef0549771c92a50
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-23 15:20:14 +0200

    Add: darkmode

--- SwiftUIDevote/SwiftUIDevoteApp.swift
+++ SwiftUIDevote/SwiftUIDevoteApp.swift
@@ -10,6 +10,8 @@ import SwiftUI
 @main
 struct SwiftUIDevoteApp: App {
     let persistenceController = PersistenceController.shared
+    
+    @AppStorage("isDarkMode") var isDarkMode: Bool = false
 
     var body: some Scene {
         WindowGroup {
@@ -19,6 +21,7 @@ struct SwiftUIDevoteApp: App {
             // To place managedobjectcontext here, it'll be injected to the all views and subviews
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .preferredColorScheme(isDarkMode ? .dark : .light)
         }
     }
 }
