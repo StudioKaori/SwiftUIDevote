d5f8ccefc283af18b5f7f25bc9584264b2dfb3f1
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-26 14:50:36 +0200

    Add: ListRowItemView

---
+++ SwiftUIDevote/View/ListRowItemView.swift
@@ -0,0 +1,24 @@
+//
+//  ListRowItemView.swift
+//  SwiftUIDevote
+//
+//  Created by Kaori Persson on 2022-09-26.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        Toggle(isOn: $item.completion, label: {
+            Text(item.task ?? "")
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundColor(item.completion ? Color.pink : Color.primary)
+                .padding(.vertical, 12)
+                .animation(.default)
+        }) //: Toggle
+    }
+}
